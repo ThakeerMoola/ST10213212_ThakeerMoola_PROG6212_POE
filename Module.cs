c93840944a88a01de79e5fd0e62056b627fc4f4f
ThakeerMoola <st10213212@rcconnect.edu.za>
2023-09-02 14:34:53 +0200

    Add project files.

---
+++ Module.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassModuleLibrary
+{
+    public class Module
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public int Credits { get; set; }
+        public int ClassHoursPerWeek { get; set; }
+
+
+        public double SelfStudyHoursPerWeek { get; set; }
+
+        public double RemainingHoursThisWeek { get; set; }
+
+        public List<StudyRecord> RecordedHours { get; set; } = new List<StudyRecord>();
+    }
+
+    public class StudyRecord
+    {
+        public DateTime Date { get; set; }
+        public double Hours { get; set; }
+    }
+}
